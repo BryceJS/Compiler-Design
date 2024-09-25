0b3c4c3fb330d50224b44af8b8c51b9822dee90b
BryceJS <143234964+BryceJS@users.noreply.github.com>
2024-09-25 16:50:08 -0700

    Add files via upload
    Lexical analyzer of my compiler.

---
+++ Assignment1.cpp
@@ -0,0 +1,9 @@
+﻿"//".*                                                         /*comment line - actions: skip */
+"int"|"real"|"if"|"then"|"else"|"while"                        {return KEYWORD; }
+[a - zA - Z][a - Za - Z0 - 9]{ 0,63 }                          {yylval.sval = strdup(yytext); trturn IDENTIFIER; }
+[=-/= <>!] +                                                   {return OPERATOR; }
+[() {}; ]                                                      {return DELIMITER; }
+[0 - 9] +                                                                             { yylval = atoi(yytext); return INT; }
+[eE][+| -] ? [0 - 9] +                                                                { yylval = atoi(yytext); return REAL; }
+\.[0-9]+                                                                              { yylval = atoi(yytext); return REAL; }
+[0 - 9] + ([eE][+| -] ? [0 - 9] + ).[0 - 9] + ([eE][+| -] ? [0 - 9] + | ε)            { yyfval = atof(yytext); return REAL; }
