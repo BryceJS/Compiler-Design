0fe5573a73e8c16ff5d4b8544305aac8f9bae607
BryceJS <143234964+BryceJS@users.noreply.github.com>
2024-09-27 13:06:26 -0700

    Add files via upload

---
+++ Assignment2/Assignment2.cpp
@@ -0,0 +1,26 @@
+﻿% {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+    %}
+
+% option noyywrap
+
+%%
+
+"//".*                  {/* skip comments */}
+"int" | "real" | "if" | "then" | "else" | "while"   { return KEYWORD; }
+[a - zA - Z][a - zA - Z0 - 9]{ 0,63 } { yylval.sval = strdup(yytext); return IDENTIFIER; }
+[=<>!] = ? { return OPERATOR; }
+[\(\)\{\}; ]             { return DELIMITER; }
+[0 - 9] + { yylval.ival = atoi(yytext); return INTNUM; }
+[0 - 9] + "."[0 - 9] * ([eE][+-] ? [0 - 9] + ) ? { yylval.fval = atof(yytext); return REALNUM; }
+%%
+
+int main(int argc, char** argv) {
+    int token;
+    while ((token = yylex()) != 0) {
+        printf("Token: %d, Value: %s\n", token, yytext);
+    }
+    return 0;
+}
